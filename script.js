cfdc4e31bb59582eba5653aee90a0325fe49ad02
soysanti44 <105685445+soysanti44@users.noreply.github.com>
2023-03-20 18:55:19 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+prediccion1=""
+prediccion2=""
+Webcam.set({
+    width:350,
+    Height:300,
+    image_format:"png",
+    png_quality:90
+})
+camera=document.getElementById("camera")
+Webcam.attach("#camera")
+function take_snapshot() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    })
+}
+
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ySr_Al52p/model.json',modelLoaded);
+
+
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+
+function speak(){
+  var synth = window.speechSynthesis;
+  speak_data_1 = "La primera prediccion es " + prediccion1;
+  speak_data_2 = "Y la segunda prediccion es " + prediccion2;
+  var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+  synth.speak(utterThis);
+}
+
+
+
+
+  function check()
+  {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+  }
+
+
+
+ function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    document.getElementById("result_emotion_name").innerHTML = results[0].label;
+    document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+    prediccion1 = results[0].label;
+    prediccion2 = results[1].label;
+    speak();
+    if(results[0].label == "like")
+    {
+      document.getElementById("update_emoji").innerHTML = "👍";
+    }
+    if(results[0].label == "love")
+    {
+      document.getElementById("update_emoji").innerHTML = "🤞";
+    }
+    if(results[0].label == "rock")
+    {
+      document.getElementById("update_emoji").innerHTML = "🤘";
+    }
+    if(results[0].label == "de chil")
+    {
+      document.getElementById("update_emoji").innerHTML = "🤙";
+    }
+
+
+    if(results[1].label == "like")
+    {
+      document.getElementById("update_emoji2").innerHTML = "👍";
+    }
+    if(results[1].label == "love")
+    {
+      document.getElementById("update_emoji2").innerHTML = "🤞";
+    }
+    if(results[1].label == "rock")
+    {
+      document.getElementById("update_emoji2").innerHTML = "🤘";
+    }
+    if(results[0].label == "de chil")
+    {
+      document.getElementById("update_emoji").innerHTML = "🤙";
+    }
+  }
+}
+
+
+
